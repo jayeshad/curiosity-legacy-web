7e50de0aea0eaf8c63df8243ea641fc8b8b0e24a
Dreamsoft <ad.jayesh@live.in>
2018-12-25 00:48:16 +0530

    added layout options and componets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainContainerComponent } from './layout/main-container/main-container.component';
+import { LeftNavbarComponent } from './layout/left-navbar/left-navbar.component';
+import { RightNavbarComponent } from './layout/right-navbar/right-navbar.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainContainerComponent,
+    LeftNavbarComponent,
+    RightNavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
