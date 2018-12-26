e2aafcab30c5365086c39af688d37fdb27e67e05
Dreamsoft <ad.jayesh@live.in>
2018-12-27 00:31:51 +0530

    remove bootstrap.js dependency initial changes

--- src/app/app.bootstrap.module.ts
+++ src/app/app.bootstrap.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { TooltipModule } from 'ngx-bootstrap/tooltip';
-import { ModalModule } from 'ngx-bootstrap/modal';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
   imports: [
     CommonModule,
-    BsDropdownModule.forRoot(),
-    TooltipModule.forRoot(),
-    ModalModule.forRoot()
+    NgbModule.forRoot()
   ],
-  exports: [BsDropdownModule, TooltipModule, ModalModule]
+  exports: []
 })
 export class AppBootstrapModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderNavbarComponent } from './layout/header-navbar/headernavbar.component';
 import { LeftNavbarComponent } from './layout/left-navbar/left-navbar.component';
 import { RightNavbarComponent } from './layout/right-navbar/right-navbar.component';
 import { FooterComponent } from './layout/footer/footer.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
   declarations: [
     AppComponent,
